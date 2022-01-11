7aea7a3b871ec507a0e6ab499d9875f1eac05e9e
Tran Dinh Duc <ducbo@yahoo.com>
2022-01-11 09:36:26 +0700

    Update system.sh

--- setup/system.sh
+++ setup/system.sh
@@ -93,16 +93,6 @@ if [ ! -f /usr/bin/add-apt-repository ]; then
 	apt_install software-properties-common
 fi
 
-# Ensure the universe repository is enabled since some of our packages
-# come from there and minimal Ubuntu installs may have it turned off.
-hide_output add-apt-repository -y universe
-
-# Install the certbot PPA.
-hide_output add-apt-repository -y ppa:certbot/certbot
-
-# Install the duplicity PPA.
-hide_output add-apt-repository -y ppa:duplicity-team/duplicity-release-git
-
 # ### Update Packages
 
 # Update system packages to make sure we have the latest upstream versions
